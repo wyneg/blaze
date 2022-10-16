f4b7ae1a7e79a29ab6050eb4acdb551492d3dabc
wyneg <wynegsrhuntar@gmail.com>
2022-10-16 18:50:37 -0300

    New page blaze

---
+++ src/public/js/caps.js
@@ -0,0 +1,96 @@
+function lucesycaps() {
+    antorchas();
+}
+
+function capsLeidos() {
+    var i;
+    for (i = 1; i <= 100; i++) {
+        if (localStorage.getItem("Capitulo" + String(i)) === String(i)) {
+            document.getElementById("modal" + String(i)).innerHTML = "Cap&iacute;tulo " + String(i) + " (leído)";
+        }
+    }
+}
+
+function antorchas() {
+    setTimeout(function () {
+        document.getElementById("izqOff").setAttribute("src", "images/Torch-izqOn.gif");
+        document.getElementById("negrito1").setAttribute("style", "opacity: 0.5");
+        document.getElementById("capitulos").setAttribute("style", "background: linear-gradient(to right, #F2F5A9, #FF8000, black");
+
+        setTimeout(function () {
+            document.getElementById("derOff").setAttribute("src", "images/Torch-derOn.gif");
+            document.getElementById("negrito1").setAttribute("style", "opacity: 0.5");
+            document.getElementById("capitulos").setAttribute("style", "background: linear-gradient(to left, #F2F5A9, #FAAC58, #FF8000");
+
+            setTimeout(function () {
+                document.getElementById("negrito1").setAttribute("style", "opacity: 1; pointer-events: all");
+                document.getElementById("capitulos").setAttribute("style", "background: transparent");
+                document.getElementById("kurobodi").setAttribute("style", "background: ''");
+            }, 400);
+        }, 1000);
+    }, 1000);
+}
+
+function pergamino(val) {
+    $('#mod1').on('hidden.bs.modal', function (e) {
+        $("#perg" + val).attr("src", "images/pergaminoquemandose.gif");
+        setTimeout(function () {
+            document.getElementById("modal" + String(val)).innerHTML = "Cap&iacute;tulo " + String(val) + " (leído)";
+        }, 2800);
+        localStorage.setItem("Capitulo" + String(val), String(val));
+    });
+}
+
+function capitulos() {
+
+    for (let x = 1; x <= 20; x++) {
+        createElements(x,1);
+    }
+
+    for (let x = 21; x <= 40; x++) {
+        createElements(x,2);
+    }
+
+    for (let x = 41; x <= 60; x++) {
+        createElements(x,3);
+    }
+
+    for (let x = 61; x <= 80; x++) {
+        createElements(x,4);
+    }
+
+    for (let x = 81; x <= 100; x++) {
+        createElements(x,5);
+    }
+
+    for (let x = 101; x <= 120; x++) {
+        createElements(x,6);
+    }
+
+    capsLeidos();
+
+}
+
+function createElements(x, y) {
+    var cap = document.createElement("A");
+    var imagen = document.createElement("IMG");
+    if(x == 11){
+        cap.textContent = "Capítulo " + x + " - Especial Halloween";
+    } else {
+        cap.textContent = "Capítulo " + x;
+    }
+    
+    cap.setAttribute("class", "list-group-item list-group-item-action list-group-item-dark text-danger mod");
+    cap.setAttribute("data-toggle", "modal");
+    cap.setAttribute("data-target", "#mod1");
+    cap.setAttribute("id", "modal" + x);
+    cap.setAttribute("onClick", "fetchgo(" + x + "); pergamino(" + x + ");");
+    
+
+    imagen.setAttribute("id", "perg" + x);
+    imagen.setAttribute("src", "images/pergaminosinquemar.gif");
+    imagen.setAttribute("alt", "pergamino");
+
+    document.getElementById("collapse"+ y).appendChild(cap);
+    document.getElementById("modal" + x).appendChild(imagen);
+}
